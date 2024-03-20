730a07d9d43242be57f3ce315654cb0cbfa93738
Timothy Karani <kartimothy@gmail.com>
2024-03-20 11:53:31 +0300

    added module factory

--- modules/Product/Tests/Models/ProductTest.php
+++ modules/Product/Tests/Models/ProductTest.php
@@ -2,13 +2,22 @@
 
 namespace Modules\Product\Tests\Models;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Product\Database\Factories\ProductFactory;
 use Modules\Product\Models\Product;
 use Modules\Product\Tests\ProductTestCase;
 
 class ProductTest extends ProductTestCase
 {
+    use RefreshDatabase;
+
     public function test_it_creates_a_product(): void
     {
+        // $product = ProductFactory::new()->create();
+        $product = Product::factory()->create();
+
+
         $product = new Product();
 
         $this->assertNotNull($product);
